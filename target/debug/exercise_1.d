e5d29f49d88effe8aab55a60c7b8216bd928b602
Sina <sina.majdieh@gmail.com>
2026-06-29 15:51:10 +0330

    initial commit

--- target/debug/exercise_1.d
+++ target/debug/exercise_1.d
@@ -1,1 +1,1 @@
-/home/sina/Documents/Coding/Rust/Learning/exercises/target/debug/exercise_1: /home/sina/Documents/Coding/Rust/Learning/exercises/chapter_8/exercise_1/src/main.rs
+/home/sina/Documents/coding/Rust/Learning/exercises/target/debug/exercise_1: /home/sina/Documents/coding/Rust/Learning/exercises/chapter_8/exercise_1/src/main.rs
